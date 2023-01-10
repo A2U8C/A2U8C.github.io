e259c24e9f003d8b5e15338441a762541baae766
A2U8C <ankushshetty95@gmail.com>
2023-01-10 20:49:07 +0530

    Update populateHTML.js

--- js/populateHTML.js
+++ js/populateHTML.js
@@ -250,20 +250,23 @@ function populateExp_Publication(items, id) {
 		spanh2.innerHTML = items[i].duration;
 
 		let h2TimelineLabel = document.createElement("h2");
-		h2TimelineLabel.innerHTML = items[i].title;
-		h2TimelineLabel.append(spanh2);
+		h2TimelineLabel.innerHTML = items[i].tit
+		
 
+
+		let a = document.createElement("a");
+		a.href = items[i].preview;
+		a.target = "_blank";
 
 		let divTimelineLabel = document.createElement("div");
+		a.append(divTimelineLabel);
 		divTimelineLabel.className = "timeline-label";
 		divTimelineLabel.append(h2TimelineLabel);
 		divTimelineLabel.append(spanTimelineSublabel);
 
 		
-		let a = document.createElement("a");
-		a.href = items[i].preview;
-		a.target = "_blank";
-		a.append(divTimelineLabel);
+		
+		
 
 
 		for (let j = 0; j < items[i].details.length; j++) {
