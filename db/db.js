986325e2fa6b566c811193994b18183cd1edbba2
A2U8C <ankushshetty95@gmail.com>
2023-03-26 09:20:19 -0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -184,7 +184,7 @@ export default {
 			duration: "May 2022 - Present",
 			subtitle: "Graduate Research Assistant",
 			details: [
-				"Worked on the ENIGMA Ontology, where coordinated for analyzing the data and generating information for documentation. Currently working on adding features to UKBB_Biobank parser using Pandas, Numpy library in python. Also working on building API for Parkinson Disease project for retrieving information about different cohorts using Flask Restful library of Python and SPARQL."
+				"Worked on the ENIGMA Ontology, where coordinated for analyzing the data and generating information for documentation. Currently working on adding features to UKBB_parser using Pandas, Numpy library in python. Also working on building API for Parkinson Disease project for retrieving information about different cohorts using Flask Restful library of Python and SPARQL."
 			],
 			tags: [
 				"Python",
